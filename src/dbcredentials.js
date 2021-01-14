d140fb53ee5826f7aee67e264263a3149fec2d89
israel diaz <israelmyname@gmail.com>
2021-01-14 15:24:33 -0500

    cambiando el ignore

---
+++ src/dbcredentials.js
@@ -0,0 +1,9 @@
+
+const  dbInfo= {
+    database:'heroku_9d5ed68f5af878d',
+    password:"84c33516",
+    host:'us-cdbr-east-03.cleardb.com',
+    user:'b6639897d64f8d'
+}
+
+module.exports = dbInfo;
